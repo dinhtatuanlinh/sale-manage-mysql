429cf1b877b39339d1c06220d6c62b917222456c
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-20 16:09:53 +0700

    add 404 page

--- sale_manage_mysql.js
+++ sale_manage_mysql.js
@@ -82,6 +82,21 @@ viewEngine(app);
 // truyền app vào route
 app.use("/", initWebRoutes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+// error handler (khi gọi url ko tồn tại trả về trang 404)
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error'); ///khi không tìm được trang sẽ trả về trang báo lỗi có thông báo lỗi đầy đủ
+    // res.render('error', { title: 'errorPage' });
+});
 
 // lấy tham số trong file .env môi trường
 let port = process.env.PORT || 6969; // ||hoặc
