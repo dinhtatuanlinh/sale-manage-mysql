5966535a14da1231d2e307769a86c37aa9078f33
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-20 16:24:55 +0700

    change viewengine static link ./public

--- config/viewEngine.js
+++ config/viewEngine.js
@@ -2,7 +2,7 @@ const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
 // khai bao bang let pham vi chi o trong file
 let configViewEngine = (app) => {
-    app.use(express.static("./src/public")); // cấu hình đường link static để khi truy cập vào ten miền sẽ truy cập thẳng vào file public
+    app.use(express.static("./public")); // cấu hình đường link static để khi truy cập vào ten miền sẽ truy cập thẳng vào file public
     // cấu hình view engine
     app.set("view engine", "ejs");
     // cấu hình đường link tới thư mục views
