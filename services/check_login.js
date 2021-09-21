79c71f181c62ecf71998e18bb3f669f86fa77fa4
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-21 16:20:42 +0700

    fix query and log profileController

--- services/check_login.js
+++ services/check_login.js
@@ -2,9 +2,10 @@ const systemConfig = require(__pathConfig + 'localVariable');
 // logging
 const logging = require(__pathServices + 'winston_logging');
 module.exports = (req, res) => {
+    logging.info(`${req.isAuthenticated()}`);
     if (!req.isAuthenticated()) { // isAuthenticated để xác định đã được login rồi hay chưa rồi sẽ trả về true chưa trả về false
         // tạo biến userInfo để truyền tới view nếu chưa đăng nhập thì đặt là rỗng
-        logging.info(`chua login`);
+
         res.locals.userInfo = '';
         let validatorErr = null;
         let registerData = { username: '', email: '' };
