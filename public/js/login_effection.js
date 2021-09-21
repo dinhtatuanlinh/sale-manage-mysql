99ac8c9a0edf96c73bf957d99279f0488b9497b2
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-21 17:30:06 +0700

    cant login

--- public/js/login_effection.js
+++ public/js/login_effection.js
@@ -6,27 +6,28 @@ if (document.getElementById('login') !== null) {
     document.getElementsByTagName("body")[0].style.alignItems = 'center';
     document.getElementsByTagName("body")[0].style.flexDirection = 'column';
 };
-
-var inputPassword = (element, id) => {
-    var hiddenInput = document.getElementById(id);
-    hiddenInput.value = MD5(element.value);
-    if (document.getElementById("registerPassword").value !== '') {
-        document.getElementById("inputRepassword").disabled = false;
-    }
-    if (id === 'registerRepassword' && document.getElementById("registerRepassword").value !== document.getElementById("registerPassword").value) {
-        document.getElementById("notice").innerHTML = "Chưa khớp với password";
-    } else {
-        document.getElementById("notice").innerHTML = '';
+window.onload = function() {
+    var inputPassword = (element, id) => {
+        var hiddenInput = document.getElementById(id);
+        hiddenInput.value = MD5(element.value);
+        if (document.getElementById("registerPassword").value !== '') {
+            document.getElementById("inputRepassword").disabled = false;
+        }
+        if (id === 'registerRepassword' && document.getElementById("registerRepassword").value !== document.getElementById("registerPassword").value) {
+            document.getElementById("notice").innerHTML = "Chưa khớp với password";
+        } else {
+            document.getElementById("notice").innerHTML = '';
+        }
+    };
+    var register = () => {
+        document.getElementById("login").classList.add("hide");
+        document.getElementById("register").classList.remove("hide");
+        if (document.getElementById("registerPassword").value === '') {
+            document.getElementById("inputRepassword").disabled = true;
+        }
+    };
+    let login = () => {
+        document.getElementById("register").classList.add("hide");
+        document.getElementById("login").classList.remove("hide");
     }
 };
-var register = () => {
-    document.getElementById("login").classList.add("hide");
-    document.getElementById("register").classList.remove("hide");
-    if (document.getElementById("registerPassword").value === '') {
-        document.getElementById("inputRepassword").disabled = true;
-    }
-};
-let login = () => {
-    document.getElementById("register").classList.add("hide");
-    document.getElementById("login").classList.remove("hide");
-}
