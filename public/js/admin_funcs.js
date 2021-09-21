694223770a5475b68b965f75555f252c44c6bceb
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-22 01:41:42 +0700

    fix admin.ejs

--- public/js/admin_funcs.js
+++ public/js/admin_funcs.js
@@ -15,7 +15,7 @@ let sendajax = (param, value, id, url) => {
     let data = { id: id, value: value }
     console.log(data, url, param);
     data = JSON.stringify(data);
-    let Dia_chi_Xu_ly = `http://${url}/admin/properties/${param}`;
+    let Dia_chi_Xu_ly = `https://${url}/admin/properties/${param}`;
     // console.log(data);
     // console.log(Dia_chi_Xu_ly);
     let XHTTP = new XMLHttpRequest() || ActiveXObject();
