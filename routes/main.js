5ce38eec42a126e3bafd8c122a3d01f319654f85
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-21 20:19:18 +0700

    fix edit profile

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const check_login = require(__pathServices + 'check_login');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => { customerDataController.customerDataPage(req, res, next) });
-router.user('/admin', check_login(req, res, next), require('./admin'));
-router.user('/profile', check_login(req, res, next), require('./profile'));
+// router.user('/admin', check_login(req, res, next), require('./admin'));
+// router.user('/profile', check_login(req, res, next), require('./profile'));
 
 module.exports = router;
