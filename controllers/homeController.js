9d8a38646d5789a2f39bdf11d9182a81844aacc4
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-22 14:05:28 +0700

    homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const systemConfig = require(__pathConfig + 'localVariable');
 const check_login = require(__pathServices + 'check_login');
 const database = require(__pathModels + "database");
@@ -9,9 +11,25 @@ let homePage = async(req, res, next) => {
         // console.log(results);
         users = results;
     });
+    let datas;
+    axios({
+        method: 'post',
+        url: 'https://betaapi.autoads.asia/PushNotification/api/contact/getcontacts',
+        data: {
+            guid: 'FB057E6D-E772-4282-9BA4-F5B6334AA66D',
+            from_date: '17/09/2021',
+            to_date: '18/09/2021'
+        }
+    }).then(function(response) {
+        datas = response;
+        logging.info("---novaon----");
+        logging.info(JSON.stringify(datas));
+    });
+
     res.setHeader("Content-Type", "text/html");
     res.render(`${systemConfig.pathInc}home`, {
-        users
+        users,
+        datas
     });
 };
 
