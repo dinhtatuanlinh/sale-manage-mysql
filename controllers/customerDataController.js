c8d410f3375af30a3316be606995645f6c07f3fb
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-09-22 23:14:23 +0700

    insert data to client_info table

--- controllers/customerDataController.js
+++ controllers/customerDataController.js
@@ -6,7 +6,13 @@ let customerDataPage = async(req, res, next) => {
     await check_login(req, res);
     // gọi biến local test ra dùng bằng cách req.app.locals.test 
     let userInfo = req.user;
-    let clientDatas = await database.Client_info.findAll({ where: { saler: userInfo.username } });
+    let clientDatas = await database.Client_info.findAll({
+        where: { saler: userInfo.username },
+        order: [
+            // Will escape title and validate DESC against a list of valid direction parameters
+            ['id', 'DESC']
+        ],
+    });
     res.setHeader("Content-Type", "text/html");
     res.render(`${systemConfig.pathInc}customer_data`, {
         userInfo,
