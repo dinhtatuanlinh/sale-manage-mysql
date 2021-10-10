8b7a268d3eab5788d783e3741b851bcf7909c97a
dinh ta tuan linh <dinhtatuanlinh@gmail.com>
2021-10-10 13:39:11 +0700

    add total customers

--- services/pending_customers.js
+++ services/pending_customers.js
@@ -1,6 +1,7 @@
 const database = require(__pathModels + "database");
 module.exports = async(userInfo) => {
     let pending_customers
+    let total_customers
     if (userInfo.role === 'admin' || userInfo.role === 'sale_manager') {
         pending_customers = await database.Client_info.count({ where: { mark: false } });
         total_customers = await database.Client_info.count();
