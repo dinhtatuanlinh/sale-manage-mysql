82fb7310261618f3b9fc51722a1958e8a31c3117
linh-dinh <linh.dinh@overdose.digital>
2022-02-11 16:49:41 +0700

    search

--- controllers/customerDataController.js
+++ controllers/customerDataController.js
@@ -63,7 +63,7 @@ let customerDataPage = async(req, res, next) => {
             {location: searchKey},
         ]}
     }else{
-        search = {[Op.is]: null}
+        search = {[Op.not]: true}
     }
 
     if (req.query.saler === undefined && userInfo.role === 'admin' || userInfo.role === 'sale_manager' ) {
