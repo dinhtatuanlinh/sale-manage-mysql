d345371c3c0d001ecb6f8bfbf26bc9004288801a
linh-dinh <linh.dinh@overdose.digital>
2022-02-11 14:43:10 +0700

    search

--- public/js/sort_animation.js
+++ public/js/sort_animation.js
@@ -85,7 +85,7 @@ let filterDate = (url, sendStatusQuery, webQuery)=>{
     
 }
 let search = (e, event, url, sendStatusQuery, webQuery, dateQuery) =>{
-    console.log(event.code);
+    console.log(event.keyCode);
     // if(event.keyCode === 13){
     //     window.location.replace(`https://${url}?search=${e.value}&ss=${sendStatusQuery}&web=${webQuery}&time=${dateQuery}`)
     // }
